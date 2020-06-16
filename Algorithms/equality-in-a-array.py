554f5863921bfc4cc34ade404a616815828c49a5
Bibhusmita <bibhusmita.patro@gmail.com>
2020-06-17 01:05:56 +0530

    Day20-21 added

---
+++ Algorithms/equality-in-a-array.py
@@ -0,0 +1,25 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+from collections import Counter
+
+# Complete the equalizeArray function below.
+def equalizeArray(arr):
+    return len(arr) - max(Counter(arr).values())
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input())
+
+    arr = list(map(int, input().rstrip().split()))
+
+    result = equalizeArray(arr)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
