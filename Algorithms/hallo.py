d84f16891d82fccc3449388642f2d11b8f837e20
Bibhusmita <bibhusmita.patro@gmail.com>
2020-06-18 23:59:34 +0530

    More solutions added.

---
+++ Algorithms/hallo.py
@@ -0,0 +1,47 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the howManyGames function below.
+def howManyGames(p, d, m, s):
+    # Return the number of games you can buy
+    count = 0
+    if s>=p:
+        s -= p
+        count +=1
+    while s>m:
+        if (p-d)>m:
+            p -= d
+            s -= p
+            count += 1
+        else:
+            s -= m
+            count += 1
+    return count
+
+    
+         
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    pdms = input().split()
+
+    p = int(pdms[0])
+
+    d = int(pdms[1])
+
+    m = int(pdms[2])
+
+    s = int(pdms[3])
+
+    answer = howManyGames(p, d, m, s)
+
+    fptr.write(str(answer) + '\n')
+
+    fptr.close()
